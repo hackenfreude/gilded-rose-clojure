a42cec884f4c11703de0baf41d9d75e30ec16b2d
hackenfreude <hackenfreude@users.noreply.github.com>
2015-06-07 20:07:09 -0700

    add some passing tests

--- test/gilded_rose/core_test.clj
+++ test/gilded_rose/core_test.clj
@@ -2,6 +2,8 @@
   (:use midje.sweet)
   (:require [gilded-rose.core :use [item update-quality]]))
 
+(def sulfuras-name "Sulfuras, Hand of Ragnaros")
+
 (defn single-item-runner [single-item]
   (let [inventory [single-item]
         result (update-quality inventory)]
@@ -10,13 +12,40 @@
 (defn make-uniform-inventory [sell-in quality]
   (let [normal (item "normal" sell-in quality)
         cheese (item "Aged Brie" sell-in quality)
-        sulfur (item "Sulfuras" sell-in quality)
+        sulfur (item sulfuras-name sell-in quality)
         passes (item "Backstage passes" sell-in quality)]
     [normal cheese sulfur passes]))
 
 
+(fact "quality doesn't go negative"
+      (let [inventory (make-uniform-inventory 0 0)
+            result (update-quality inventory)
+            qualities (map :quality result)]
+        qualities => (has not-any? neg?)
+        (count qualities) => 4))
 
-;(fact "a normal item is normal"
+(fact "sell-in decreases each day except sulfuras"
+      (let [inventory (make-uniform-inventory 1 1)
+            non-sulf-inv (remove (fn [x] (= (:name x) sulfuras-name)) inventory)
+            result (update-quality non-sulf-inv)
+            sellins (map :sell-in result)]
+        sellins => (has every? zero?)
+        (count sellins) => 3))
+
+(fact "sulfuras "
+      (let [sulfuras (item sulfuras-name 0 80)
+            result (single-item-runner sulfuras)]
+        (fact "sell-in stays the same"
+              (:sell-in result) => 0)
+        (fact "quality stays the same"
+              (:quality result) => 80)))
+
+
+
+
+
+
+;(fact "a normal item is normal
 ;      (let [inventory [(item "something" 2 2)]
 ;        result (update-quality inventory)
 ;        updated-item (first result)]
@@ -34,12 +63,6 @@
 ;        (fact "quality decrements by 1"
 ;              (:quality result) => 9)))
 
-(fact "quality doesn't go negative"
-      (let [inventory (make-uniform-inventory 0 0)
-            result (update-quality inventory)
-            qualities (map :quality result)]
-        qualities => (has not-any? neg?)
-        (count qualities) => 4))
 
 ;(fact "sell-in decrements by 1"
 ;      (let [inventory (make-uniform-inventory 1 0)]
